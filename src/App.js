1b4b8eca8180c96b4a95cb3150a2dd9b36a06da1
Carolina Adriana Hidalgo Avila <carolina.hidalgo@ucb.edu.bo>
2021-08-26 15:57:06 -0400

    rebanada_2

--- src/App.js
+++ src/App.js
@@ -1,1 +1,9 @@
 
+const nombre = document.querySelector("#nombre-input");
+const form = document.querySelector("#saludador-form");
+
+form.addEventListener("submit", event => {
+    alert(nombre.value);
+
+})
+
