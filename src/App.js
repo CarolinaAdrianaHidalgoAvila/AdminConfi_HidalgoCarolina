590282f710609f9a6cbcb22f38014eaaa1c1ee0b
Carolina Adriana Hidalgo Avila <carolina.hidalgo@ucb.edu.bo>
2021-08-26 16:37:46 -0400

    rebanada_4

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 
 const nombre = document.querySelector("#nombre-input");
 const edad = document.querySelector("#edad-input");
+const genero = document.querySelector("#genero-input");
 const form = document.querySelector("#saludador-form");
 
 form.addEventListener("submit", event => {
     mensaje = "Hola ";
-  if(edad.value>30){
-      mensaje = mensaje + "señor(a)";
-  }
+    hombreJoven = "joven ";
+    hombreAdulto = "señor ";
+    mujerJoven = "señorita ";
+    mujerAdulta = "señora ";
 
+  if(edad.value>30){
+      if(genero.value == "Mujer"){
+        mensaje = mensaje + mujerAdulta;
+      }else{
+        mensaje = mensaje + hombreAdulto;
+      }
+  }else{
+    if(genero.value == "Mujer"){
+        mensaje = mensaje + mujerJoven;
+      }else{
+        mensaje = mensaje + hombreJoven;
+      }
+  }
     alert(mensaje + nombre.value);
 
 });
