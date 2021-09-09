3824f2ad1cd5be35e0ce2b23122a0aa16d3079f0
Carolina Adriana Hidalgo Avila <carolina.hidalgo@ucb.edu.bo>
2021-09-09 10:12:10 -0400

    agregar tarea

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 
 const tarea = document.querySelector("#tarea-input");
-const tareaTipo = document.querySelector("#tipoTarea");
 const form = document.querySelector("#Tareas-form");
 const tarea_output = document.querySelector("#show-tarea");
-const tipo_output = document.querySelector("#show-tipo");
 form.addEventListener("submit", event => {
   event.preventDefault();
   tarea_output.innerHTML = "Tarea: " + tarea.value;
-  tipo_output.innerHTML = "Tipo: " + tareaTipo.value;
   
 });
 
