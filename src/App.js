7abff1755ddb5ed3d9f62926db87ce5167378a9c
Carolina Adriana Hidalgo Avila <carolina.hidalgo@ucb.edu.bo>
2021-09-09 09:39:08 -0400

    tipo Tarea

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 
 const tarea = document.querySelector("#tarea-input");
-const form = document.querySelector("#totalizer-form");
+const tareaTipo = document.querySelector("#tarea-input");
+const form = document.querySelector("#Tareas-form");
 const tarea_output = document.querySelector("#show-tarea");
 const tipo_output = document.querySelector("#show-tipo");
 form.addEventListener("submit", event => {
   event.preventDefault();
   tarea_output.innerHTML = "Tarea: " + tarea.value;
-  tipo_output.innerHTML = "Tipo: " + tarea.value;
+  tipo_output.innerHTML = "Tipo: " + tareaTipo.value;
   
 });
 
